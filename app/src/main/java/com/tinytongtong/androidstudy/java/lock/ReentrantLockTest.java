ab886f07a49183575a63f65b077e67fb8b8b4ea2
wangjianzhou <tinytongtong@163.com>
2020-08-26 16:11:13 +0800

    更新代码

---
+++ app/src/main/java/com/tinytongtong/androidstudy/java/lock/ReentrantLockTest.java
@@ -0,0 +1,48 @@
+package com.tinytongtong.androidstudy.java.lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @Description: TODO
+ * @Author wangjianzhou@qding.me
+ * @Date 2020/8/25 3:45 PM
+ * @Version TODO
+ */
+public class ReentrantLockTest {
+    int a = 0;
+    ReentrantLock lock = new ReentrantLock();
+
+    public void writer() {
+        lock.lock();
+        try {
+            a++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void reader() {
+        lock.lock();
+        try {
+            int i = a;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantLockTest reentrantLockTest = new ReentrantLockTest();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                reentrantLockTest.writer();
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                reentrantLockTest.reader();
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/com/tinytongtong/androidstudy/java/multithread/MultiThreadTest.java
@@ -0,0 +1,13 @@
+package com.tinytongtong.androidstudy.java.multithread;
+
+/**
+ * @Description: 查看一个普通的Java程序包含哪些线程
+ * @Author wangjianzhou@qding.me
+ * @Date 2020/8/25 6:16 PM
+ * @Version
+ */
+public class MultiThreadTest {
+    public static void main(String[] args) {
+
+    }
+}
