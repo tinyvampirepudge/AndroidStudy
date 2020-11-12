381e5343ee0a5d64dfc4fdaae9d88aaa90f2299b
wangjianzhou <tinytongtong@163.com>
2020-11-12 23:03:43 +0800

    kotlin可见性修饰符

---
+++ app/src/main/java/com/tinytongtong/androidstudy/kotlin/visibilitymodifiers/VisibilityModifiersTest.kt
@@ -0,0 +1,19 @@
+package com.tinytongtong.androidstudy.kotlin.visibilitymodifiers
+
+/**
+ * @Description: 可见性修饰符
+ * https://www.kotlincn.net/docs/reference/visibility-modifiers.html
+ *
+ * @Author tinytongtong
+ * @Date 2020/11/12 9:59 PM
+ * @Version
+ */
+
+/**
+ * 类、对象、接口、构造函数、方法、属性和它们的 setter 都可以有 可见性修饰符。
+ * （getter 总是与属性有着相同的可见性。）
+ * 在 Kotlin 中有这四个可见性修饰符：private、 protected、 internal 和 public。
+ * 如果没有显式指定修饰符的话，默认可见性是 public。
+ *
+ * 请注意在 Kotlin 中，外部类不能访问内部类的 private 成员。
+ */
