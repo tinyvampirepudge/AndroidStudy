5ebfdcc948c98daab4b701452f38fe57e9e642ef
wangjianzhou <didi@bogon.didichuxing.com>
2020-12-17 10:23:48 +0800

    retrofit

---
+++ app/src/main/java/com/tinytongtong/androidstudy/retrofit/RetrofitActivity.kt
@@ -0,0 +1,19 @@
+package com.tinytongtong.androidstudy.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tinytongtong.androidstudy.R
+
+/**
+ * @Description: retrofit源码学习
+ *
+ * @Author tinytongtong
+ * @Date 2020/12/8 5:13 PM
+ * @Version
+ */
+class RetrofitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_retrofit)
+    }
+}
