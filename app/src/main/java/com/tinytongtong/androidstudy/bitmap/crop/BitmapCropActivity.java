f33805fb013cc0f1ede70dca0aac10eb72d85a6e
wangjianzhou <wangjianzhou@didichuxing.com>
2021-03-18 17:49:25 +0800

    更新代码

---
+++ app/src/main/java/com/tinytongtong/androidstudy/bitmap/crop/BitmapCropActivity.java
@@ -0,0 +1,23 @@
+package com.tinytongtong.androidstudy.bitmap.crop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tinytongtong.androidstudy.R;
+
+/**
+ * @Description: TODO
+ *
+ * @Author tinytongtong
+ * @Date 3/16/21 11:07 AM
+ * @Version TODO
+ */
+public class BitmapCropActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitmap_crop);
+    }
+}
