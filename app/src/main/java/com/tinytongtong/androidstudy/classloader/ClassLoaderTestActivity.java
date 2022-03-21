f29dbdaf29016e9dbb2f2860ee42ac2969317755
wangjianzhou <wangjianzhou@didichuxing.com>
2022-03-21 18:52:58 +0800

    热修复基础

--- app/src/main/java/com/tinytongtong/androidstudy/classloader/ClassLoaderTestActivity.java
+++ app/src/main/java/com/tinytongtong/androidstudy/classloader/ClassLoaderTestActivity.java
@@ -1,10 +1,5 @@
 package com.tinytongtong.androidstudy.classloader;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
@@ -17,7 +12,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.tinytongtong.androidstudy.MainActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.tinytongtong.androidstudy.R;
 
 import java.io.File;
